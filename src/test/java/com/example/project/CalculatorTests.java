4b6a3be3d00d85956335846c5dfa2246b86f58e3
Babrah <ybabrah@deloitte.com>
2019-04-30 17:56:38 +0530

    Test Case Change

--- src/test/java/com/example/project/CalculatorTests.java
+++ src/test/java/com/example/project/CalculatorTests.java
@@ -25,18 +25,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 class CalculatorTests {
 	// important params for TWB Logging
 	public String testPlanKey = "TP-10883";
-	public String testCaseKey = "TC-84601";
+	public String testCaseKey = "TC-84606";
 	
 	@Test
 	@DisplayName("1 + 1 = 2")
-	@Disabled
 	void addsTwoNumbers() {
 		Calculator calculator = new Calculator();
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 	}
 
 	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@Disabled
 	@CsvSource({
 			"0,    1,   1",
 			"1,    2,   3",
